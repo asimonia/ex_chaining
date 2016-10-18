6d37d1ff24b0ffb2b42b7da089fef63d7864f6ba
asimonia <alex.simonian@gmail.com>
2016-10-18 17:53:08 -0500

    Off the chain

---
+++ chaining.js
@@ -0,0 +1,24 @@
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+
+function filterIt(element, index, array) {
+	if (element <= 19) {
+		return element;
+	}
+}
+
+function mapIt(element, index, array) {
+	var another = element * 1.5 - 1;
+	return another;
+}
+
+// Sort the numbers in descending order (10, 9, 8, 7, etc).
+// Use difference function
+// Remove any integers greater than 19.
+// Use the filter
+// Multiply each remaining number by 1.5 and then subtract 1.
+// Use the map function
+// Chain em
+
+integers.sort(function(a, b) {return b - a}).filter(filterIt).map(mapIt);
+
